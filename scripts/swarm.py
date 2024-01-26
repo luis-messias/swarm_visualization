8340f617af51ad168c573eb3228d0fdb9d3399dc
Luis Messias <luisfelipe_messias@hotmail.com>
2024-01-26 17:11:57 -0300

    Refactoring publish and subscribe functions

--- scripts/swarm.py
+++ scripts/swarm.py
@@ -13,24 +13,16 @@ from visualization_msgs.msg import MarkerArray
 import numpy as np
 import copy
 
-class swarmClass():   
+class SwarmClass():   
 
     def __init__(self):
         # SWARM name
         self.swarm_name = rospy.get_name()[1:]
         print("Init swarm: ", self.swarm_name)
-        now = rospy.get_rostime()
-        self.t_last_update = now.secs +  now.nsecs/1000000000
-
 
         # Getting ROSPARAM
         self.fixed_frame = str(rospy.get_param("/" + self.swarm_name + '/frame',"map"))
         self.robots_number = int(rospy.get_param("/" + self.swarm_name + '/num_robots',1))
-        robot_mesh_path = str(rospy.get_param("/" + self.swarm_name + '/mesh_file',"package://swarm_visualization/meshes/test.stl"))
-
-        self.hit_radius = float(rospy.get_param("/" + self.swarm_name + '/hit_radius',1.1))
-        self.warning_radius = float(rospy.get_param("/" + self.swarm_name + '/warning_radius',2))
-        #self.cmd_time_out = float(rospy.get_param("/" + self.swarm_name + '/cmd_time_out',1))
         
         #Velocity and aceleration limiter
         self.has_velocity_limiter = bool(rospy.get_param("/" + self.swarm_name + '/has_velocity_limiter', 0))
@@ -49,9 +41,73 @@ class swarmClass():
 
         self.robots_velocity_vx = np.zeros(self.robots_number)
         self.robots_velocity_wz = np.zeros(self.robots_number)
-        self.velocity_vx_setpoit = np.zeros(self.robots_number)
-        self.velocity_wz_setpoit = np.zeros(self.robots_number)
+        
 
+        for i in range(self.robots_number):
+            self.robots_position_x[i] = rospy.get_param("/" + self.swarm_name + '/x_' + str(i),0)
+            self.robots_position_y[i] = rospy.get_param("/" + self.swarm_name + '/y_' + str(i),0)
+
+
+    def update_distance_matrix(self):
+        for i in range(self.robots_number):
+            for j in range(self.robots_number): 
+                if i == j:
+                    self.robots_distance_matrix[i][j] = np.inf
+                else:
+                    self.robots_distance_matrix[i][j] =  (self.robots_position_x[i] - self.robots_position_x[j]) ** 2
+                    self.robots_distance_matrix[i][j] += (self.robots_position_y[i] - self.robots_position_y[j]) ** 2
+                    self.robots_distance_matrix[i][j] =   self.robots_distance_matrix[i][j] ** 0.5
+    
+    def update_velocity_limiter(self, dt, velocity_vx_setpoit, velocity_wz_setpoit):
+        #Limiting acceleration
+        if self.has_acceleration_limiter:
+            Dvx_max = self.max_acc_x * dt
+            Dwz_max = self.max_acc_z * dt
+            self.robots_velocity_vx = np.clip(velocity_vx_setpoit, self.robots_velocity_vx - Dvx_max, self.robots_velocity_vx + Dvx_max)
+            self.robots_velocity_wz = np.clip(velocity_wz_setpoit, self.robots_velocity_wz - Dwz_max, self.robots_velocity_wz + Dwz_max)
+        else:
+            self.robots_velocity_vx = velocity_vx_setpoit
+            self.robots_velocity_wz = velocity_wz_setpoit
+
+        #Limiting velocity
+        if self.has_velocity_limiter:
+            self.robots_velocity_vx = np.clip(self.robots_velocity_vx, -self.max_vx, self.max_vx)
+            self.robots_velocity_wz = np.clip(self.robots_velocity_wz, -self.max_wz, self.max_wz)
+        else:
+            self.robots_velocity_vx = velocity_vx_setpoit
+            self.robots_velocity_wz = velocity_wz_setpoit
+
+    def integrate_system(self, dt):
+        self.robots_position_x   += self.robots_velocity_vx*np.cos(self.robots_position_yaw)*dt
+        self.robots_position_y   += self.robots_velocity_vx*np.sin(self.robots_position_yaw)*dt
+        self.robots_position_yaw += self.robots_velocity_wz*dt
+
+    def update(self, dt, velocity_vx_setpoit, velocity_wz_setpoit):
+        self.update_velocity_limiter(dt, velocity_vx_setpoit, velocity_wz_setpoit)
+        self.integrate_system(dt)
+        self.update_distance_matrix()
+
+    def get_state(self):
+        return  (self.robots_position_x,
+                self.robots_position_y,
+                self.robots_position_yaw,
+                self.robots_velocity_vx,
+                self.robots_velocity_wz,
+                self.robots_distance_matrix)
+    
+class RosPublish():
+    def __init__(self):
+        # SWARM name
+        self.swarm_name = rospy.get_name()[1:]
+        print("Init swarm Publisher: ", self.swarm_name)
+
+        # Getting ROSPARAM
+        self.fixed_frame = str(rospy.get_param("/" + self.swarm_name + '/frame',"map"))
+        self.robots_number = int(rospy.get_param("/" + self.swarm_name + '/num_robots',1))
+        robot_mesh_path = str(rospy.get_param("/" + self.swarm_name + '/mesh_file',"package://swarm_visualization/meshes/test.stl"))
+
+        self.hit_radius = float(rospy.get_param("/" + self.swarm_name + '/hit_radius',1.1))
+        self.warning_radius = float(rospy.get_param("/" + self.swarm_name + '/warning_radius',2))        
         self.marker_array = MarkerArray()
 
         # Initializing arrays
@@ -69,11 +125,7 @@ class swarmClass():
         initial_marker.color.b = 0.0
 
         for i in range(self.robots_number):
-            self.robots_position_x[i] = rospy.get_param("/" + self.swarm_name + '/x_' + str(i),0)
-            self.robots_position_y[i] = rospy.get_param("/" + self.swarm_name + '/y_' + str(i),0)
-            
             initial_marker.id = i
-
             self.marker_array.markers.append(copy.deepcopy(initial_marker))
 
         # Publishers
@@ -81,17 +133,45 @@ class swarmClass():
         self.odom_publisher_list = []
         for id in range(self.robots_number):
             self.odom_publisher_list.append(rospy.Publisher("/" + self.swarm_name + "/robot_" + str(id) + "/odom", Odometry, queue_size = 10))
+    
+    def publish_tf(self, robots_position_x, robots_position_y, robots_position_yaw):
+        tf_broadcast = tf2_ros.TransformBroadcaster()
+        transform = geometry_msgs.msg.TransformStamped()
         
-        #Subscriber
-        self.twister_subcriber_list = []
-        for id in range(self.robots_number):
-            self.twister_subcriber_list = rospy.Subscriber("/" + self.swarm_name + "/robot_" + str(id) + "/cmd_vel",
-                                                Twist, self.callback_twist, id)
+        transform.header.frame_id = self.fixed_frame
+        transform.header.stamp = rospy.Time.now()        
 
-    def callback_twist(self, msg, i):
-        self.velocity_vx_setpoit[i] = msg.linear.x
-        self.velocity_wz_setpoit[i] = msg.angular.z
+        for i in range(self.robots_number):
+            transform.child_frame_id = self.swarm_name + "_" + str(i)
 
+            transform.transform.translation.x  = robots_position_x[i]
+            transform.transform.translation.y = robots_position_y[i]
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, robots_position_yaw[i])
+            transform.transform.rotation.x = quaternion[0]
+            transform.transform.rotation.y = quaternion[1]
+            transform.transform.rotation.z = quaternion[2]
+            transform.transform.rotation.w = quaternion[3]
+
+            tf_broadcast.sendTransform(transform)
+    
+    def publish_odom(self, robots_position_x, robots_position_y, robots_position_yaw, robots_velocity_vx, robots_velocity_wz):
+        odom = Odometry()
+        odom.header.frame_id = self.fixed_frame
+        odom.header.stamp = rospy.Time.now()
+        for i in range(self.robots_number):
+            odom.pose.pose.position.x = robots_position_x[i]
+            odom.pose.pose.position.y = robots_position_y[i]
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, robots_position_yaw[i])
+            odom.pose.pose.orientation.x = quaternion[0]
+            odom.pose.pose.orientation.y = quaternion[1]
+            odom.pose.pose.orientation.z = quaternion[2]
+            odom.pose.pose.orientation.w = quaternion[3]
+
+            odom.twist.twist.linear.x = robots_velocity_vx[i]
+            odom.twist.twist.angular.z = robots_velocity_wz[i]
+
+            self.odom_publisher_list[i].publish(odom) 
+    
     def set_marker_green(this, marker):
         marker.color.r = 0.0
         marker.color.g = 1.0
@@ -107,20 +187,20 @@ class swarmClass():
         marker.color.g = 1.0
         marker.color.b = 0.0
     
-    def publish_marker_array(self):
+    def publish_marker_array(self, robots_position_x, robots_position_y, robots_position_yaw, robots_distance_matrix):
         for i in range(self.robots_number):
             self.marker_array.markers[i].header.stamp = rospy.Time.now()
-            self.marker_array.markers[i].pose.position.x = self.robots_position_x[i]
-            self.marker_array.markers[i].pose.position.y = self.robots_position_y[i]
+            self.marker_array.markers[i].pose.position.x = robots_position_x[i]
+            self.marker_array.markers[i].pose.position.y = robots_position_y[i]
             
-            if np.min(self.robots_distance_matrix[i]) < self.hit_radius:
+            if np.min(robots_distance_matrix[i]) < self.hit_radius:
                 self.set_marker_red(self.marker_array.markers[i])
-            elif np.min(self.robots_distance_matrix[i]) < self.warning_radius:
+            elif np.min(robots_distance_matrix[i]) < self.warning_radius:
                 self.set_marker_yellow(self.marker_array.markers[i])
             else:
                 self.set_marker_green(self.marker_array.markers[i])
 
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, self.robots_position_yaw[i] - np.pi/2)
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, robots_position_yaw[i] - np.pi/2)
             self.marker_array.markers[i].pose.orientation.x = quaternion[0]
             self.marker_array.markers[i].pose.orientation.y = quaternion[1]
             self.marker_array.markers[i].pose.orientation.z = quaternion[2]
@@ -128,90 +208,46 @@ class swarmClass():
 
         self.marker_publisher.publish(self.marker_array)
 
-    def publish_odom(self):
-        odom = Odometry()
-        odom.header.frame_id = self.fixed_frame
-        odom.header.stamp = rospy.Time.now()
-        for i in range(self.robots_number):
-            odom.pose.pose.position.x = self.robots_position_x[i]
-            odom.pose.pose.position.y = self.robots_position_y[i]
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, self.robots_position_yaw[i])
-            odom.pose.pose.orientation.x = quaternion[0]
-            odom.pose.pose.orientation.y = quaternion[1]
-            odom.pose.pose.orientation.z = quaternion[2]
-            odom.pose.pose.orientation.w = quaternion[3]
+    def publish_all(self, robot_state):
+        (robots_position_x,
+        robots_position_y,
+        robots_position_yaw,
+        robots_velocity_vx,
+        robots_velocity_wz,
+        robots_distance_matrix) = robot_state
+        self.publish_tf(robots_position_x,
+                        robots_position_y,
+                        robots_position_yaw)    
+        self.publish_odom(robots_position_x,
+                        robots_position_y,
+                        robots_position_yaw,
+                        robots_velocity_vx,
+                        robots_velocity_wz)
+        self.publish_marker_array(robots_position_x,
+                        robots_position_y,
+                        robots_position_yaw,
+                        robots_distance_matrix) 
 
-            odom.twist.twist.linear.x = self.robots_velocity_vx[i]
-            odom.twist.twist.angular.z = self.robots_velocity_wz[i]
+class RosSubscriber():
+    def __init__(self):
+          # SWARM name
+        self.swarm_name = rospy.get_name()[1:]
+        print("Init swarm subscriber: ", self.swarm_name)
+        self.robots_number = int(rospy.get_param("/" + self.swarm_name + '/num_robots',1))
 
-            self.odom_publisher_list[i].publish(odom)
-
-    def publish_tf(self):
-        tf_broadcast = tf2_ros.TransformBroadcaster()
-        transform = geometry_msgs.msg.TransformStamped()
+        #Subscriber
+        self.twister_subcriber_list = []
+        for id in range(self.robots_number):
+            self.twister_subcriber_list = rospy.Subscriber("/" + self.swarm_name + "/robot_" + str(id) + "/cmd_vel",
+                                                Twist, self.callback_twist, id)
         
-        transform.header.frame_id = self.fixed_frame
-        transform.header.stamp = rospy.Time.now()        
+        self.velocity_vx_setpoit = np.zeros(self.robots_number)
+        self.velocity_wz_setpoit = np.zeros(self.robots_number)
 
-        for i in range(self.robots_number):
-            transform.child_frame_id = self.swarm_name + "_" + str(i)
+    def callback_twist(self, msg, i):
+        self.velocity_vx_setpoit[i] = msg.linear.x
+        self.velocity_wz_setpoit[i] = msg.angular.z
 
-            transform.transform.translation.x  = self.robots_position_x[i]
-            transform.transform.translation.y = self.robots_position_y[i]
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, self.robots_position_yaw[i])
-            transform.transform.rotation.x = quaternion[0]
-            transform.transform.rotation.y = quaternion[1]
-            transform.transform.rotation.z = quaternion[2]
-            transform.transform.rotation.w = quaternion[3]
-
-            tf_broadcast.sendTransform(transform)
-
-    def publish_all(self):
-        self.publish_marker_array()
-        self.publish_odom()
-        self.publish_tf()
-
-    def update_distance_matrix(self):
-        for i in range(self.robots_number):
-            for j in range(self.robots_number): 
-                if i == j:
-                    self.robots_distance_matrix[i][j] = np.inf
-                else:
-                    self.robots_distance_matrix[i][j] =  (self.robots_position_x[i] - self.robots_position_x[j]) ** 2
-                    self.robots_distance_matrix[i][j] += (self.robots_position_y[i] - self.robots_position_y[j]) ** 2
-                    self.robots_distance_matrix[i][j] =   self.robots_distance_matrix[i][j] ** 0.5
-    
-    def update_velocity_limiter(self, dt):
-        #Limiting acceleration
-        if self.has_acceleration_limiter:
-            Dvx_max = self.max_acc_x * dt
-            Dwz_max = self.max_acc_z * dt
-            self.robots_velocity_vx = np.clip(self.velocity_vx_setpoit, self.robots_velocity_vx - Dvx_max, self.robots_velocity_vx + Dvx_max)
-            self.robots_velocity_wz = np.clip(self.velocity_wz_setpoit, self.robots_velocity_wz - Dwz_max, self.robots_velocity_wz + Dwz_max)
-        else:
-            self.robots_velocity_vx = self.velocity_vx_setpoit
-            self.robots_velocity_wz = self.velocity_wz_setpoit
-
-        #Limiting velocity
-        if self.has_velocity_limiter:
-            self.robots_velocity_vx = np.clip(self.robots_velocity_vx, -self.max_vx, self.max_vx)
-            self.robots_velocity_wz = np.clip(self.robots_velocity_wz, -self.max_wz, self.max_wz)
-        else:
-            self.robots_velocity_vx = self.velocity_vx_setpoit
-            self.robots_velocity_wz = self.velocity_wz_setpoit
-
-    def integrate_system(self, dt):
-        self.robots_position_x   += self.robots_velocity_vx*np.cos(self.robots_position_yaw)*dt
-        self.robots_position_y   += self.robots_velocity_vx*np.sin(self.robots_position_yaw)*dt
-        self.robots_position_yaw += self.robots_velocity_wz*dt
-
-    def update(self, ):
-        now = rospy.get_rostime()
-        dt = now.secs +  now.nsecs/1000000000 - self.t_last_update
-        self.t_last_update += dt
-
-        #print("Update: ", dt)
-        self.update_velocity_limiter(dt)
-        self.integrate_system(dt)
-        self.update_distance_matrix()
-        self.publish_all()
+    def get_velocity_set_point(self):
+        return (self.velocity_vx_setpoit,
+               self.velocity_wz_setpoit)
--- scripts/swarm_node.py
+++ scripts/swarm_node.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
 import rospy
-from swarm import swarmClass
-
-
-
-
-# def update_pose(this,dt):
-#     if (rospy.Time.now() - this.cmd_time).secs > 0.5:
-#         this.vx = 0
-#         this.wz = 0
-
+from swarm import SwarmClass, RosPublish, RosSubscriber
 
 rospy.init_node("swarm_name")
 
-swarm = swarmClass()
-
+swarm = SwarmClass()
+publisher = RosPublish()
+subscriber = RosSubscriber()
+dt = 0.02
 while True:
-    swarm.update()
-    rospy.sleep(0.02)
+    (velocity_vx_setpoit,
+    velocity_wx_setpoit) = subscriber.get_velocity_set_point()
+    swarm.update(dt, velocity_vx_setpoit, velocity_wx_setpoit)
+    robot_state = swarm.get_state()
+    publisher.publish_all(robot_state)  
+
+
+    rospy.sleep(dt)
 
 
